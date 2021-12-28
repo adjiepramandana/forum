c879d7c5cc0d29963d9eb61109b20d7bf99213ab
adjiepramandana <68036145+adjiepramandana@users.noreply.github.com>
2021-12-28 16:38:29 +0700

    Add files via upload

---
+++ class.php
@@ -0,0 +1,47 @@
+<?php
+class AzThuFan {
+
+function AzSpam($url,$msg){
+    global $y;
+
+        $asedekon = array(
+            'pesan'=>$msg,
+            'subjek'=>$subjek
+        );
+
+        $samalusemua = http_build_query($asedekon);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$samalusemua);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+
+        $exec = curl_exec($ch);
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        if($status == 200)
+        {
+        return "[✔] ➝ Succeed 「".$status."」Spam To ".$y." 「Done」 \n";
+        }
+        if($status == 0 || $status == 404)
+        {
+        return "[☠] ➝ Website Down「".$status."」Spam To ".$y." 「Failed」 \n";
+        }
+        if($status == 502 || $status == 504 || $status == 500)
+        {
+        return "[✘] ➝ Gateway Timeout 「".$status."」Spam To ".$y."「Failed」 \n";
+        }
+        if($status == 302 || $status == 301 || $status == 307)
+        {
+        return "[➡️] ➝ Redirect「".$status."」Spam To ".$y."「Failed」 \n";
+        }
+        if($status == 100 || $status == 101 || $status == 417 || $status == 505 || $status == 522 || $status == 400)
+        {
+        return "[✘] ➝ Error 「".$status."」Spam To ".$y."「Failed」 \n";
+        }
+    }
+}
+?>
